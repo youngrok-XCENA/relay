a41e81161ed738c5a21cc364069d131e6eb5a02b
youngrok-XCENA <yr.song@metisx.com>
2026-04-04 17:06:11 +0900

    fix: What changed 섹션 제거, PR fix comment preamble 제거
    - PR body 출력 형식을 Why + Design changes(mermaid)만 유지
    - PR fix comment에도 첫 # 헤딩 이전 preamble 제거 적용

--- tests/test_codex_auto_pipeline_prfix_comment.sh
+++ tests/test_codex_auto_pipeline_prfix_comment.sh
@@ -19,8 +19,8 @@ WORKFLOW_CONTENT="$(cat "$CODEX_WORKFLOW")"
 
 assert_contains "$WORKFLOW_CONTENT" "- name: Post PR fix comment"
 assert_contains "$WORKFLOW_CONTENT" "if: steps.prfix_push.outputs.has_changes == 'true'"
-assert_contains "$WORKFLOW_CONTENT" 'body: ${{ steps.prfix.outputs.result }}'
-assert_contains "$WORKFLOW_CONTENT" 'target: ${{ steps.pr.outputs.number }}'
-assert_contains "$WORKFLOW_CONTENT" "type: pr"
+assert_contains "$WORKFLOW_CONTENT" 'PRFIX_RESULT: ${{ steps.prfix.outputs.result }}'
+assert_contains "$WORKFLOW_CONTENT" 'PR_NUMBER: ${{ steps.pr.outputs.number }}'
+assert_contains "$WORKFLOW_CONTENT" 'gh pr comment'
 
 echo "ok"
